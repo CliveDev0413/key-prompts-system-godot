f6dc77612d40c33c628ea71e287ac322d2a8b428
Clive <anoodleofcode@gmail.com>
2022-02-21 18:57:46 +0800

    initial commit

---
+++ Key Prompt System/C#/KeyPrompt.cs
@@ -0,0 +1,307 @@
+using Godot;
+using System;
+using static Godot.GD;
+using Newtonsoft.Json;
+using System.Collections.Generic;
+
+public class KeyPrompt : Node2D
+{
+	enum Controllers
+	{
+		ps4,
+		xboxOne,
+	}
+
+	private bool usingKeyboard = true;
+	private Controllers connectedController;
+
+	[Export]
+	public string action = "";
+
+	[Export]
+	public string keyboardFramesJsonPath = "res://ButtonPromptTextures/keyboardFrames.json";
+
+	[Export]
+	public string ps4FramesJsonPath = "res://ButtonPromptTextures/ps4Frames.json";
+
+	[Export]
+	public string xboxOneFramesJsonPath = "res://ButtonPromptTextures/xboxOneFrames.json";
+
+	[Export]
+	public bool lightThemedKeybrdKeys = false;
+
+	private string controllerName = "none";
+
+	Dictionary<string, int> keyboard = new Dictionary<string, int>();
+	Dictionary<string, int> buttons = new Dictionary<string, int>();
+
+	//Sprites
+	private Sprite blankKey;
+	private Sprite keybrdDark;
+	private Sprite keybrdLight;
+	private Sprite ps4;
+	private Sprite xboxOne;
+
+	//JSON Files
+	private File keyboardFramesJson;
+	private File ps4FramesJson;
+	private File xboxOneFramesJson;
+
+	//JSON Files but text
+	private string keyboardJsonToText;
+	private string ps4JsonToText;
+	private string xboxOneJsonToText;
+
+	public override void _Ready()
+	{
+		//Load the json files
+		LoadJsonFiles();
+
+		//Convert the json to text 
+		//(cause from what I know, newtonsoft json only converts jsons when its a string)
+		ConvertAllJsonToText();    
+		
+		//Get all the sprites
+		blankKey = GetNode<Sprite>("Blank");
+		keybrdDark = GetNode<Sprite>("Keyboard_Dark");
+		keybrdLight = GetNode<Sprite>("Keyboard_Light");
+		ps4 = GetNode<Sprite>("PS4");
+		xboxOne = GetNode<Sprite>("Xbox_One");
+
+		//Get the frames of the keys
+		keyboard = JsonConvert.DeserializeObject<Dictionary<string, int>>(keyboardJsonToText);    
+	}
+
+	public override void _Input(InputEvent inputEvent)
+	{
+		//if the input is from a keyboard
+		if(inputEvent is InputEventKey)
+		{
+			usingKeyboard = true;
+			controllerName = "none";
+		}
+		//if the input is from a controller
+		else if(inputEvent is InputEventJoypadButton || inputEvent is InputEventJoypadMotion)
+		{
+			usingKeyboard = false;
+			controllerName = Input.GetJoyName(Input.GetConnectedJoypads().IndexOf(inputEvent.Device));
+
+			//Gets the type of controller (Playstation, Xbox, Nintendo, etc)
+			getControllerType(controllerName);
+		}
+
+		processInputs(controllerName);
+	}
+
+	private void processInputs(string _controllerName)
+	{
+		//The inputs from the action in the input map
+		Godot.Collections.Array inputs; 
+
+		try
+		{
+			inputs = InputMap.GetActionList(action);
+		}
+		catch(Exception err)
+		{
+			PushError($"The action variable is not assigned: {err}");
+			GetTree().Quit();
+			return;
+		}
+
+		//If the action supports controller or if it reacts to controller input
+		bool hasControllerInput = false;
+
+		string keyName = "";
+		InputEventJoypadButton buttonProperties = null;
+		InputEventJoypadMotion joystickProperties = null;
+		InputEventMouseButton mouseProperties = null;
+		//InputEventMouseMotion mouseMovementProperties = null;
+
+
+		//Check the inputs in the action
+		foreach(InputEvent input in inputs)
+		{
+			if(input is InputEventKey)
+			{
+				keyName = input.AsText().ToLower();
+			}
+			else if(input is InputEventJoypadButton)
+			{
+				buttonProperties = (InputEventJoypadButton)input;
+				hasControllerInput = true;
+			}
+			else if(input is InputEventJoypadMotion)
+			{
+				joystickProperties = (InputEventJoypadMotion)input;
+				hasControllerInput = true;
+			}
+			else if(input is InputEventMouseButton)
+			{
+				mouseProperties = (InputEventMouseButton)input;
+			}
+		}
+
+		hideAllPrompts();
+
+		if(usingKeyboard)
+		{
+			if(lightThemedKeybrdKeys)
+			{
+				keybrdLight.Visible = true;
+
+				if(mouseProperties != null)
+				{
+					keybrdLight.Frame = keyboard[mouseButtonIndexToButtonName(mouseProperties.ButtonIndex).ToLower()];
+					return;
+				}
+
+				keybrdLight.Frame = keyboard[keyName];
+			}
+			else
+			{
+				keybrdDark.Visible = true;
+
+				if(mouseProperties != null)
+				{
+					keybrdDark.Frame = keyboard[mouseButtonIndexToButtonName(mouseProperties.ButtonIndex).ToLower()];
+					return;
+				}
+
+				keybrdDark.Frame = keyboard[keyName];
+			}
+		}
+		//If using a controller and the action supports controller
+		else if(!usingKeyboard && hasControllerInput)
+		{
+			if(connectedController == Controllers.ps4)
+			{
+				ps4.Visible = true;
+
+				if(joystickProperties != null)
+				{
+					if(joystickProperties.Axis == 0 || joystickProperties.Axis == 1)
+					{
+						ps4.Frame = buttons["left-stick"];
+					}
+					else if(joystickProperties.Axis == 2 || joystickProperties.Axis == 3)
+					{
+						ps4.Frame = buttons["right-stick"];
+					}
+
+					return;
+				}
+
+				ps4.Frame = buttons[buttonProperties.ButtonIndex.ToString()];
+			}
+			else if(connectedController == Controllers.xboxOne)
+			{
+				xboxOne.Visible = true;
+
+				if(joystickProperties != null)
+				{
+					if(joystickProperties.Axis == 0 || joystickProperties.Axis == 1)
+					{
+						xboxOne.Frame = buttons["left-stick"];
+					}
+					else if(joystickProperties.Axis == 2 || joystickProperties.Axis == 3)
+					{
+						xboxOne.Frame = buttons["right-stick"];
+					}
+
+					return;
+				}
+
+				xboxOne.Frame = buttons[buttonProperties.ButtonIndex.ToString()];
+			}
+			else
+			{
+				blankKey.Visible = true;
+			}
+		}
+	}
+
+	private void getControllerType(string _controllerName)
+	{
+		switch(_controllerName.ToLower())
+		{
+			case "ps4 controller":
+				buttons = JsonConvert.DeserializeObject<Dictionary<string, int>>(ps4JsonToText);
+				connectedController = Controllers.ps4;
+				break;
+
+			case "xbox one controller":
+				buttons = JsonConvert.DeserializeObject<Dictionary<string, int>>(xboxOneJsonToText);
+				connectedController = Controllers.xboxOne;
+				break;
+
+			case "xinput gamepad":
+				buttons = JsonConvert.DeserializeObject<Dictionary<string, int>>(xboxOneJsonToText);
+				connectedController = Controllers.xboxOne;
+				break;
+
+			default:
+				buttons = JsonConvert.DeserializeObject<Dictionary<string, int>>(xboxOneJsonToText);
+				connectedController = Controllers.xboxOne;
+				break;
+		}
+	}
+
+	private void hideAllPrompts()
+	{
+		blankKey.Visible = false;
+		keybrdDark.Visible = false;
+		keybrdLight.Visible = false;
+		ps4.Visible = false;
+		xboxOne.Visible = false;
+	}
+
+	private void LoadJsonFiles()
+	{
+		keyboardFramesJson = new File();
+		ps4FramesJson = new File();
+		xboxOneFramesJson = new File();
+
+		keyboardFramesJson.Open(keyboardFramesJsonPath, File.ModeFlags.Read);
+		ps4FramesJson.Open(ps4FramesJsonPath, File.ModeFlags.Read);
+		xboxOneFramesJson.Open(xboxOneFramesJsonPath, File.ModeFlags.Read);
+
+		if(!keyboardFramesJson.FileExists(keyboardFramesJsonPath) ||
+		   !ps4FramesJson.FileExists(ps4FramesJsonPath) ||
+		   !xboxOneFramesJson.FileExists(xboxOneFramesJsonPath))
+		{
+			PushError("One of the json file paths are not assigned.");
+			GetTree().Quit();
+		}
+	}
+
+	private void ConvertAllJsonToText()
+	{
+		keyboardJsonToText = keyboardFramesJson.GetAsText();
+		ps4JsonToText = ps4FramesJson.GetAsText();
+		xboxOneJsonToText = xboxOneFramesJson.GetAsText();
+	}
+
+	private string mouseButtonIndexToButtonName(int buttonIndex)
+	{
+		switch(buttonIndex)
+		{
+			case 1:
+				return "BUTTON_LEFT";
+			case 2:
+				return "BUTTON_RIGHT";
+			case 3:
+				return "BUTTON_MIDDLE";
+			case 4:
+				return "BUTTON_WHEEL_UP";
+			case 5:
+				return "BUTTON_WHEEL_DOWN";
+			case 6:
+				return "BUTTON_WHEEL_LEFT";
+			case 7:
+				return "BUTTON_WHEEL_RIGHT";
+			default:
+				return "invalid";
+		}
+	}
+}
---
+++ Key Prompt System/GDScript/KeyPrompt.gd
@@ -0,0 +1,186 @@
+extends Node2D
+
+enum Controllers{
+	ps4,
+	xboxOne,
+}
+
+var using_keyboard: bool;
+var connected_controller;
+
+export(String) var ACTION = "";
+
+export(String) var KEYBOARD_FRAMES_JSON_PATH = "res://Key Prompt System/ButtonPromptTextures/keyboardFrames.json";
+export(String) var PS4_FRAMES_JSON_PATH = "res://Key Prompt System/ButtonPromptTextures/ps4Frames.json";
+export(String) var XBOX_ONE_FRAMES_JSON_PATH = "res://Key Prompt System/ButtonPromptTextures/xboxOneFrames.json";
+
+export(bool) var LIGHT_THEMED_KEYBRD_KEYS: bool;
+
+var controller_name: String;
+
+var keyboard: Dictionary;
+var buttons: Dictionary;
+
+# Sprites
+onready var blank_key = $Blank;
+onready var keybrd_dark = $Keyboard_Dark;
+onready var keybrd_light = $Keyboard_Light;
+onready var ps4 = $PSFour;
+onready var xbox_one = $Xbox_One;
+
+# JSON Files
+var keyboard_frames_json: File;
+var ps4_frames_json: File;
+var xbox_one_frames_json: File;
+
+# JSON Files but text
+var keyboard_json_to_text: String;
+var ps4_json_to_text: String;
+var xbox_one_json_to_text: String;
+
+
+func _ready():
+	load_json_files();
+	convert_all_json_to_text();
+	
+	keyboard = parse_json(keyboard_json_to_text);
+
+func _input(event):
+	if event is InputEventKey:
+		using_keyboard = true;
+		controller_name = "none";
+	elif event is InputEventJoypadButton || event is InputEventJoypadMotion:
+		using_keyboard = false;
+		controller_name = Input.get_joy_name(Input.get_connected_joypads().find(event.device))
+
+		get_controller_type(controller_name);
+	
+	process_inputs(controller_name);
+
+func process_inputs(_controller_name):
+	var inputs;
+
+	assert(ACTION != "", "The action variable is not assigned.");
+
+	inputs = InputMap.get_action_list(ACTION);
+
+	var has_controller_input: bool;
+
+	var key_name: String = "";
+	var button_properties: InputEventJoypadButton = null;
+	var joystick_properties: InputEventJoypadMotion = null;
+	var mouse_properties: InputEventMouseButton = null;
+	
+	for input in inputs:
+		if input is InputEventKey:
+			key_name = input.as_text().to_lower();
+		elif input is InputEventJoypadButton:
+			button_properties = input;
+			has_controller_input = true;
+		elif input is InputEventJoypadMotion:
+			joystick_properties = input;
+			has_controller_input = true;
+		elif input is InputEventMouseButton:
+			mouse_properties = input;
+	
+	hide_all_prompts();
+
+	if using_keyboard:
+		if LIGHT_THEMED_KEYBRD_KEYS:
+			keybrd_light.visible = true;
+
+			if mouse_properties != null:
+				keybrd_light.frame = keyboard[mouse_button_index_to_name(mouse_properties.button_index).to_lower()];
+				return;
+
+			keybrd_light.frame = keyboard[key_name];
+		else:
+			keybrd_dark.visible = true;
+
+			if mouse_properties != null:
+				keybrd_dark.frame = keyboard[mouse_button_index_to_name(mouse_properties.button_index).to_lower()];
+				return;
+
+			keybrd_dark.frame = keyboard[key_name];
+	elif !using_keyboard && has_controller_input:
+		if connected_controller == Controllers.ps4:
+			ps4.visible = true;
+
+			if joystick_properties != null:
+				if joystick_properties.axis == 0 || joystick_properties.axis == 1:
+					ps4.frame = buttons["left-stick"];
+				elif joystick_properties.axis == 2 || joystick_properties.axis == 3:
+					ps4.frame = buttons["right-stick"];
+				return;
+		
+			ps4.frame = buttons[str(button_properties.button_index)];
+		elif connected_controller == Controllers.xboxOne:
+			xbox_one.visible = true;
+
+			if joystick_properties != null:
+				if joystick_properties.axis == 0 || joystick_properties.axis == 1:
+					xbox_one.frame = buttons["left-stick"];
+				elif joystick_properties.axis == 2 || joystick_properties.axis == 3:
+					xbox_one.frame = buttons["right-stick"];
+				return;
+		
+			xbox_one.frame = buttons[str(button_properties.button_index)];	
+
+func get_controller_type(_controller_name: String):
+	match(_controller_name.to_lower()):
+		"ps4 controller":
+			buttons = parse_json(ps4_json_to_text);
+			connected_controller = Controllers.ps4;
+		"xbox one controller":
+			buttons = parse_json(xbox_one_json_to_text);
+			connected_controller = Controllers.xboxOne;
+		"xinput gamepad":
+			buttons = parse_json(xbox_one_json_to_text);
+			connected_controller = Controllers.xboxOne;
+		_:
+			buttons = parse_json(xbox_one_json_to_text);
+			connected_controller = Controllers.xboxOne;
+
+func hide_all_prompts():
+	blank_key.visible = false;
+	keybrd_dark.visible = false;
+	keybrd_light.visible = false;
+	ps4.visible = false;
+	xbox_one.visible = false;
+
+func load_json_files():
+	keyboard_frames_json = File.new();
+	ps4_frames_json = File.new();
+	xbox_one_frames_json = File.new();
+
+	assert(keyboard_frames_json.file_exists(KEYBOARD_FRAMES_JSON_PATH), "The variable 'KEYBOARD_FRAMES_JSON_PATH' is not assigned");
+	assert(ps4_frames_json.file_exists(PS4_FRAMES_JSON_PATH), "The variable 'PS4_FRAMES_JSON_PATH' is not assigned");
+	assert(xbox_one_frames_json.file_exists(XBOX_ONE_FRAMES_JSON_PATH), "The variable 'PS4_FRAMES_JSON_PATH' is not assigned");
+
+	keyboard_frames_json.open(KEYBOARD_FRAMES_JSON_PATH, File.READ);
+	ps4_frames_json.open(PS4_FRAMES_JSON_PATH, File.READ);
+	xbox_one_frames_json.open(XBOX_ONE_FRAMES_JSON_PATH, File.READ);
+
+func convert_all_json_to_text():
+	keyboard_json_to_text = keyboard_frames_json.get_as_text();
+	ps4_json_to_text = ps4_frames_json.get_as_text();
+	xbox_one_json_to_text = xbox_one_frames_json.get_as_text();
+
+func mouse_button_index_to_name(button_index: int):
+	match(button_index):
+		1:
+			return "BUTTON_LEFT";
+		2:
+			return "BUTTON_RIGHT";
+		3:
+			return "BUTTON_MIDDLE";
+		4:
+			return "BUTTON_WHEEL_UP";
+		5:
+			return "BUTTON_WHEEL_DOWN";
+		6:
+			return "BUTTON_WHEEL_LEFT";
+		7:
+			return "BUTTON_WHEEL_RIGHT";
+		_:
+			return "invalid";
